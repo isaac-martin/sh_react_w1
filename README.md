ccd1af25cbda222fd81c8f755ac367402587421d
isaac-martin <isaac@weareplayground.co>
2018-05-16 09:26:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sh_react_w1
---
+++ js/app.js
@@ -0,0 +1,42 @@
+const Highlight = ({ color, children }) => (
+  <span className={`relative highlight highlight-${color}`}>
+    <span className="relative z-2">{children}</span>
+  </span>
+)
+
+const Intro = () => <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+  <div className="mb3 mb4-ns"><Highlight color='aqua'>Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color='yellow'>explore</Highlight>, in <Highlight color='blue'>Tokyo, </Highlight>Japan. </div>
+  <div>From museums and galleries, to Robot Restaurants and kitten cafes, Tokyo is the gift that keeps on giving. Dattebayo!</div>
+</div>
+
+const NavItem = ({ className, href, children, logo }) => (
+  <li className={`nh2-ns f6 f4-1 tc ${className}`}>
+    <a className='white no-underline' href="{href}">
+      {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
+
+    </a>
+  </li>
+)
+const Nav = () => (
+  <nav className='pt3 pt4-ns nb4 mb0-ns'>
+    <ul className='list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 na0'>
+      {menu.map(item =>
+        <NavItem {...item} />
+      )}
+    </ul>
+  </nav>
+)
+
+const App = () => (
+  <div>
+    <div className="min-vh-100 ph4 flex flex-column">
+      <Nav />
+      <Intro />
+    </div>
+    <div className="flex flex-wrap container">{/* our attractions list component */}</div>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
